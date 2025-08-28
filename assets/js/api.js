546191b7188a007deaab003183566cb65a7f8157
ramondiass <ramondiass0507@gmail.com>
2025-08-28 17:10:51 -0300

    update api.js and main.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/ramondiass/js-developer-portfolio/refs/heads/main/data/profile.json"
+    const url = "https://raw.githubusercontent.com/ramondiass/js-developer-portfolio/refs/heads/main/data/teste.json"
     const fetching = await fetch(url)
     return await fetching.json()
 }
