839a01caf631854a5340c04375e70bb48235f4b0
ramondiass <ramondiass0507@gmail.com>
2025-08-28 16:34:28 -0300

    teste

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const url = "https://raw.githubusercontent.com/ramondiass/js-developer-portfolio/refs/heads/main/data/profile.json"
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
